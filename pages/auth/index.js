0f26a712bb60d025b0e1062dd785789d0984a734
Ahmed Bargady <bargadyahmed2000@gmail.com>
2022-01-06 16:55:15 +0100

    structure frontend.

---
+++ pages/auth/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Index() {
+  return (
+    <div>
+      <Head>
+        <title>Jina Web3.0 Blockchain | Auth</title>
+      </Head>
+      <div></div>
+    </div>
+  );
+}
+
+export default Index;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Jina Web3.0 Blockchain App</title>
+      </Head>
+      <div></div>
+    </div>
+  );
+}
